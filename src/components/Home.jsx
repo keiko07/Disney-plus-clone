f3805bfa22600f7a40258fbc97740a4bd2567ccf
Kevin Bermudez <kevinber1999@outlook.com>
2021-11-11 09:49:24 -0500

    Add img slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import { ImgSlider } from './ImgSlider'
+import { Viewers } from './Viewers'
 
 export const Home = () => {
     return (
-        <div>
-            hola 2:42:00
-        </div>
+        <Container>
+            <ImgSlider />
+            <Viewers />
+        </Container>
     )
 }
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(2vw + 5px);
+    position: relative;
+    overflow-x: hidden;
+
+    &:before {
+        background: url("/images/home-background.png") center center / cover
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+
+`
